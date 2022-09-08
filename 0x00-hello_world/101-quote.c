b526c78ca8e2df8ba03ce685b9992e8a4ccb2acf
Adamu Muhammad <sayyid211@gmail.com>
2022-09-08 15:33:24 +0000

    print to stderr

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	fprintf("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	return (0);
+}
