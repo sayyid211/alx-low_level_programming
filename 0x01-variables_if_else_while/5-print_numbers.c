dd36b6af53e69e71062b3b0ad337e4d9fc4e68b3
Adamu Muhammad <sayyid211@gmail.com>
2022-09-09 16:45:26 +0000

    print 0 to 9

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * x: variable
+ * Return: Success
+ */
+
+int main(void)
+{
+	int x;
+
+	for (x = 0; x < 10; x++)
+	{
+		putchar(x);
+	}
+	putchar('\n');
+	return (0);
+}
