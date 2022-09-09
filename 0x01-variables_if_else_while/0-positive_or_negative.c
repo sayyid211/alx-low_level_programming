014396103952177a3c85a3cfcd62ed6f0b6e47cc
Adamu Muhammad <sayyid211@gmail.com>
2022-09-09 14:12:23 +0000

    number line position

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -11,15 +11,15 @@ int main(void)
 	int n;
 	if (n <= 0)
 	{
-		printf("%d, is negative",&n);
+		printf("%ls, is negative",&n);
 	}
 	else if (n == 0)
 	{
-		printf("%d, is zero", &n);
+		printf("ls, is zero", &n);
 	}
 	else
 	{
-		printf("%d, is positive", &n);
+		printf("ls, is positive", &n);
 	}
 	return (0);
 }
