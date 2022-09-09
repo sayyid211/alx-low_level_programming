6558571fc5bc1dbe1250d7a21bc6ec589fd560dd
Adamu Muhammad <sayyid211@gmail.com>
2022-09-09 17:21:49 +0000

    print hexadecimal

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * x: 0-9
+ * y: a-f
+ * Return: success
+ */
+
+int main(void)
+{
+	char x, y;
+
+	x = 0;
+	y = a;
+	while (x <= 9)
+	{
+		putchar('x');
+		x++;
+	}
+	while (y <= f)
+	{
+		putchar('y');
+		y++;
+	}
+	putchar('\n');
+	return (0);
+}
