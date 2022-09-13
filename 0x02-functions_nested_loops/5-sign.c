00092bec5bee65ec69ee2ff7df2c8d7f49294be0
Adamu Muhammad <sayyid211@gmail.com>
2022-09-13 14:39:53 +0000

    check sign

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * print_sign - returns the sign of a value
+ * @n: argument
+ * Return: 0, + or -
+ */
+
+int print_sign(int n)
+{
+	if (n < 0)
+	{
+		_putchar('-');
+		return (-1);
+	}
+	else if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else
+	{
+		_putchar('0');
+		return (0);
+	}
+}
