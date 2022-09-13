bea29e37b29a5b767fbe3aa585225bc5bf9923b5
Adamu Muhammad <sayyid211@gmail.com>
2022-09-13 11:42:54 +0000

    put char

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,13 +1,11 @@
 #include "main.h"
 /**
  * putchar - print characters to stdout
- * i: character to print
- * str: chars for x to loop through
- * Return: Success
  */
-int putchar(int)
+
+void putchar(void)
 {
-	char xint i = 0;
+	int i = 0;
 	char str[] = "_putchar";
 
 	while (str[i] != '\n')
@@ -15,6 +13,14 @@ int putchar(int)
 		_putchar(str[i]);
 		i++;
 	}
+}
+/**
+ * main - Entry point
+ * Return: Success
+ */
+int main(void)
+{
+	putchar();
 	_putchar('\n');
 	return (0);
 }
