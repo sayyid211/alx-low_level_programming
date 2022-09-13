b580844adfb01e2e6eb64f6925d35f9cde127b6b
Adamu Muhammad <sayyid211@gmail.com>
2022-09-13 14:16:59 +0000

    isalpha

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * _isalpha - checks if character is aA to zZ
+ * @c: argument to test
+ * Return: true or false
+ */
+
+int _isalpha(int c)
+{
+	return ((c > 96 < 123) && (c > 64 < 91));
+}
