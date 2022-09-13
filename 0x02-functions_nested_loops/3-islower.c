3cd9d921800fb5685876accb7b8f9f3eae024520
Adamu Muhammad <sayyid211@gmail.com>
2022-09-13 13:12:40 +0000

    islower

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * islowerm - checks for the case of an alphabet
+ * Return: lower(0) 0r upper(1)
+ */
+
+int islower(int c)
+{
+	if (c <97)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
