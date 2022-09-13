9a2d201ffeb25cc7d1fd400d7b43a5bfbfe45aa6
Adamu Muhammad <sayyid211@gmail.com>
2022-09-13 22:24:52 +0000

    ret last digit

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,6 +1,25 @@
 #include "main.h"
 
 /**
+ * _abs - returns the absolute value of an integer
+ *
+ * @x: arg
+ * Return: abs val
+ */
+
+int _abs(int x)
+{
+	if (x < 0)
+	{
+		return (-1 * x);
+	}
+	else
+	{
+		return (x);
+	}
+}
+
+/**
  * print_last_digit - return the last digit of an integer
  * @x: arg
  * Return: last digit
