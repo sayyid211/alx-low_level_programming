ee2c6c8fb3da2c5ea0a8174b07d53b3b8735fcba
Adamu Muhammad <sayyid211@gmail.com>
2022-09-13 12:48:13 +0000

    print 10 x abc

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -9,13 +9,14 @@ void print_alphabet_x10(void)
 	int x = 97;
 	int row = 0;
 
-	while(row < 9)
+	while (row < 9)
 	{
-		while(x < 123)
+		while (x < 123)
 		{
 			_putchar(x);
 			x++;
 		}
 		_putchar('\n');
+		row++;
 	}
 }
