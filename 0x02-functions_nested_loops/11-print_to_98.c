aaf0e10959a7a40e57d69e106292c5de56f86db3
Adamu Muhammad <sayyid211@gmail.com>
2022-09-14 03:44:16 +0000

    print to 98

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -12,16 +12,16 @@ void print_to_98(int n)
 	{
 		if (n > 98)
 		{
-			printf("%d, ", n);
+			printf("%i, ", n);
 			n -= 1;
 		}
 		else if (n == 98)
 		{
-			printf("%d, ", n);
+			printf("%i, ", n);
 		}
 		else
 		{
-			printf("%d, ", n);
+			printf("%i, ", n);
 			n += 1;
 		}
 	}
