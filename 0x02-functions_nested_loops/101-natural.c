6ca70a20b3bcc4c609eb7f610ef6c8ffde6b5211
Adamu Muhammad <sayyid211@gmail.com>
2022-09-14 04:01:01 +0000

    sum mutls of 3 n 5

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -10,9 +10,9 @@ int main(void)
 	int x = 0;
 	int sum = 0;
 
-	for (;x < 1024; x++)
+	for (; x < 1024; x++)
 	{
-		if ((x % 3 == 0) && (x % 5 == 0))
+		if ((x % 3 == 0) || (x % 5 == 0))
 			sum += x;
 	}
 	printf("%d\n", sum);
