863fa819dc132d8c9b69a2eaaf6d8c56e0af4fba
Adamu Muhammad <sayyid211@gmail.com>
2022-09-15 15:00:09 +0000

    print nums !2,4

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_most_numbers - print nums excluding 2 n 4
+ */
+
+void print_most_numbers(void)
+{
+	int i;
+
+	for (i = 48; i < 58; i++)
+	{
+		if ('i' != 4 && 'i' != 2)
+			_putchar(i);
+		continue;
+	}
+}
