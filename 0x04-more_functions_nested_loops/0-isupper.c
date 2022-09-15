d00d11b804015ed0df6c649efb1429950f3088ed
Adamu Muhammad <sayyid211@gmail.com>
2022-09-15 13:45:44 +0000

    is upper

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _isupper - checks for upper case alphabets
+ * @c: arg
+ * Return: Success
+ */
+
+int _isupper(int c)
+{
+	if (c > (65 + 25))
+		return (0);
+	else
+		return (1);
+}
