35ee64d7ab3f88478bbb4f07c1dc102f58dcca6e
Adamu Muhammad <sayyid211@gmail.com>
2022-09-16 12:03:41 +0000

    square

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -9,14 +9,22 @@ void print_square(int size)
 	int i = 0;
 	int j = 0;
 
-	while (i < size)
+	if (size <= 0)
 	{
-		while (j < size)
-		{
-			_putchar('\#');
-			j++;
-		}
-		j = 0;
 		_putchar('\n');
 	}
+	else
+	{
+		while (i < size)
+		{
+			while (j < size)
+			{
+				_putchar('\#');
+				j++;
+			}
+			j = 0;
+			_putchar('\n');
+		}
+	}
+	_putchar('\n');
 }
