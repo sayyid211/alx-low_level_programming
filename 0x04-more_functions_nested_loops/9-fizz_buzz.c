a3c2e4e642a722b2b5ac474d3896738863ebbffd
Adamu Muhammad <sayyid211@gmail.com>
2022-09-18 22:54:30 +0000

    fizz buzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -11,15 +11,15 @@ int main(void)
 
 	while (x <= 100)
 	{
-		if (x % 2 == 0 && x % 3 == 0)
+		if (x % 3 == 0 && x % 5 == 0)
 		{
 			printf("FizzBuzz ");
 		}
-		else if (x % 2 == 0)
+		else if (x % 3 == 0)
 		{
 			printf("Fizz ");
 		}
-		else if (x % 3 == 0)
+		else if (x % 5 == 0)
 		{
 			printf("Buzz ");
 		}
