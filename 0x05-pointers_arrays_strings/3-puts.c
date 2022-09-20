7dd05286d0a7354ed6cd64380533603565f13ec0
Adamu Muhammad <sayyid211@gmail.com>
2022-09-20 01:51:48 +0000

    puts

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _puts - print str
+ * @str: arg
+ */
+
+void _puts(char *str)
+{
+	int counter = 0;
+
+	while (*(str + counter) != 0)
+	{
+		_putchar(str);
+		counter++;
+	}
+}
