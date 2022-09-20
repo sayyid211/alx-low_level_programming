5bc56db141fddc06250891dbf0b4125da08bde46
Adamu Muhammad <sayyid211@gmail.com>
2022-09-20 02:10:02 +0000

    rev str

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * _strlen - computes the length of a string
+ * @s: argument, pointer int to string array
+ * Return: size of string array
+ */
+int _strlen(char *s)
+{
+	int i = 0;
+
+	while (*(s + i) != 0)
+	{
+		i++;
+	}
+	return (i);
+}
+
+void print_rev(char *s)
+{
+	int length = _strlen(*s);
+
+	while (length >= 0)
+	{
+		_putchar(*(s + length));
+		length--;
+	}
+	_putchar('\n');
+}
