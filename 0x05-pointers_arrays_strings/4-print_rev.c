f074f612b0e72e32c646d339097072723eea7b0d
Adamu Muhammad <sayyid211@gmail.com>
2022-09-20 02:19:49 +0000

    rev string

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -22,11 +22,12 @@ int _strlen(char *s)
 
 void print_rev(char *s)
 {
-	int length = _strlen(s);
+	int length = _strlen(s) - 1;
 
 	while (length >= 0)
 	{
 		_putchar(*(s + length));
 		length--;
 	}
+	_putchar('\n');
 }
