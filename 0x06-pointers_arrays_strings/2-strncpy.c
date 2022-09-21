6402ba447a6e8866564850ead0b3c57e9404c4e3
Adamu Muhammad <sayyid211@gmail.com>
2022-09-21 15:34:33 +0000

    copy string

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * _strncpy - copy str
+ * @dest: receiving buffer
+ * @src: source
+ * @n: size limit
+ * Return: copy of src
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i = 0; /* iterator fot indeces */
+
+	while (*src)
+	{
+		if (i < n)
+		{
+			dest[i] = src[i];
+			i++;
+		}
+		else
+		{
+			break;
+		}
+	}
+	return (dest);
+}
