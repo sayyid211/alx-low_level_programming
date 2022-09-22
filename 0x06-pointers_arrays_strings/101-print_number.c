d736fd7ae16cb2546ae5c5b1f06c1f0da17570f1
Adamu Muhammad <sayyid211@gmail.com>
2022-09-22 11:59:54 +0000

    print integer with putchar

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -17,8 +17,8 @@ void print_number(int n)
 
 	/* remove the last digit and recur */
 
-	if (n/10)
-		print_number(n/10);
+	if (n / 10)
+		print_number(n / 10);
 
 	/* print last digit */
 	_putchar((n % 10) + '0');
