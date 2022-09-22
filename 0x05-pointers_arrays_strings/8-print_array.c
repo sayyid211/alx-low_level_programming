fb1e1473ee14ac5aea05a31d673547284735e5dc
Adamu Muhammad <sayyid211@gmail.com>
2022-09-22 16:10:54 +0000

    print int array

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_array - print part of an array
+ * @a: array
+ * @n: number if items to print
+ */
+
+void print_array(int *a, int n)
+{
+	int i = 0; /* itrator val */
+
+	while (i < (n - 1))
+	{
+		printf("%d, ", a[i]);
+		i++;
+	}
+	printf("%d\n", a[n - 1]);
+}
