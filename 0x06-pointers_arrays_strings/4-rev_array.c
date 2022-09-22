69b7834c9d5ddd810263922c99ad009fc3933760
Adamu Muhammad <sayyid211@gmail.com>
2022-09-22 10:13:36 +0000

    reverse array

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * reverse_array - reverse an array
+ * @a: the array's pointer
+ * @n: size of the array
+ */
+
+void reverse_array(int *a, int n)
+{
+	int i = 0;
+	int temp;
+
+	while (i < n/2)
+	{
+		temp = a[i];
+		a[i] = a[n - i - 1];
+		a[n - i - 1] = temp;
+		i++;
+	}
+}
