1816a5e89a3b1ea5e659bb6335e78a82282a333d
Adamu Muhammad <sayyid211@gmail.com>
2022-09-22 17:05:35 +0000

    copy complete string

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,36 @@
+#include "main.h"
+
+/**
+ * _strcpy - copy all str
+ * @dest: destination
+ * @src: source
+ * Return: duplicate
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	int i = 0; /* iterator */
+
+	while (i < _strlen(src))
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	return (dest);
+}
+
+/**
+ * _strlen - computes the length of a string
+ * @s: argument, pointer int to string array
+ * Return: size of string array
+ */
+int _strlen(char *s)
+{
+	int i = 0;
+
+	while (*(s + i) != 0)
+	{
+		i++;
+	}
+	return (i);
+}
