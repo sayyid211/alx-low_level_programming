079655fedd68eebee2e480e0442279ee38cc0ba3
Adamu Muhammad <sayyid211@gmail.com>
2022-09-22 11:33:12 +0000

    print int

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_number - prints a number
+ * @n: arg
+ */
+
+void print_number(int n)
+{
+	/* if n is negative print a minus */
+
+	if (n < 0)
+	{
+		_putchar('-');
+		n *= -1;
+	}
+
+	/* remove the last digit and recur */
+
+	if (n/10)
+		print_number(n/10);
+
+	/* print last digit */
+	_putchar((n % 10) + '0');
+}
