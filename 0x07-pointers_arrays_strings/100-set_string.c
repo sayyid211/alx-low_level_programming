70268f24d16fcaa26ef6e7b1b68123bdc56766ea
Adamu Muhammad <sayyid211@gmail.com>
2022-09-27 17:16:33 +0000

    set str

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * set_string - set a string to char
+ * @s: pointer to receiving string
+ * @to:token to send
+ */
+
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
