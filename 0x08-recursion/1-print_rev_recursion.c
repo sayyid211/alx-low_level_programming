fe3e947099796fd515ebc3f70eaaa6edffd50158
Adamu Muhammad <sayyid211@gmail.com>
2022-09-29 05:57:04 +0000

    print reverse str with recursion

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _print_rev_recursion - recursively print a string in reverse
+ * @s: arg
+ */
+
+void _print rev_recursion(char *s)
+{
+	if (*s)
+	{
+		_print_rev_recursion(s + 1);
+		_putchar(*s);
+	}
+}
