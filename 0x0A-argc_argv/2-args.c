fee76f8e418074bb39770eea0fae18edff84f1ca
Adamu Muhammad <sayyid211@gmail.com>
2022-09-30 21:40:15 +0000

    print all args

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * @argc: args count
+ * @argv: args vector
+ * Return: Success
+ */
+
+int main(int argc, char *argv[])
+{
+	int count;
+
+	for (count = 0; count < argc; count++)
+	{
+		printf("%s\n", argv[count]);
+	}
+	return (0);
+}
