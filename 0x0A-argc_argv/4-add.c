0d0a532579c629e5590605db358668c38a8fbe4b
Adamu Muhammad <sayyid211@gmail.com>
2022-09-30 22:00:45 +0000

    args add

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,41 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * @argc: arg count
+ * @argv: arg vect
+ * Return: sum of args
+ */
+
+int main(int argc, char **argv)
+{
+	int i = 1;
+	int y;
+	int sum = 0;
+
+	for (; i < argc; i++)
+	{
+		int x = atoi(argv[i]);
+
+		if (!x)
+		{
+			printf("0\n");
+			y = 0;
+		}
+
+		else if (__builtin_types_compatible_p(typeof(x), int))
+		{
+			sum += x;
+			y = 0;
+		}
+		else
+		{
+			printf("Error\n");
+			y = 1;
+			break;
+		}
+	}
+	printf("%d\n", sum);
+	return (y);
+}
