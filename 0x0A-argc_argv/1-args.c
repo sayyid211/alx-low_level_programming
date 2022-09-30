f7d20b2c17226c0ce55696e4cc9dccfd858367b8
Adamu Muhammad <sayyid211@gmail.com>
2022-09-30 21:32:16 +0000

    arg c

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * @argc: arguments count 
+ * @argv: arguments vector
+ * Return: Success
+ */
+
+int main(int argc, __attribute__((unused)) char *argv[])
+{
+	int count = 0;
+
+	while (count < argc)
+	{
+		count++;
+	}
+	printf("%d\n", count);
+	return (0);
+}
