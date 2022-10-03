91f961a50b102ba8ba573aa704cb2aed0128b54c
Adamu Muhammad <sayyid211@gmail.com>
2022-10-03 20:14:30 +0000

    update malloc concatenation

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -28,7 +28,7 @@ char *str_concat(char *s1, char *s2)
 		}
 		else
 		{
-			p[i] = NULL;
+			p[i] = "";
 		}
 		i = _strlen(s1);
 		if (s2 != NULL)
@@ -40,7 +40,7 @@ char *str_concat(char *s1, char *s2)
 		}
 		else
 		{
-			p[i + j] = NULL;
+			p[i + j] = "";
 		}
 		p[i + j + 1] = '\0';
 		return (p);
