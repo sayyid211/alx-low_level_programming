63ade4958d143bc73349cec709b063a1741504af
Adamu Muhammad <sayyid211@gmail.com>
2022-10-03 20:23:39 +0000

    update malloc concatenation

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -17,8 +17,6 @@ char *str_concat(char *s1, char *s2)
 	while (1)
 	{
 		p = malloc((_strlen(s1) + _strlen(s2) + 1) * sizeof(char));
-		if (p == NULL)
-			return (NULL);
 		for (i = 0; i < _strlen(s1); i++)
 		{
 			p[i] = s1[i];
