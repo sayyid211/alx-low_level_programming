533b0dcfaf62ecdf44376ce5dea1b82dddf1a28d
Adamu Muhammad <sayyid211@gmail.com>
2022-10-10 17:51:59 +0000

    define dog

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,9 +1,17 @@
 #ifndef DOG_H
 #define DOG_H
+/**
+ * struct dog - dog struct
+ * @name: name of dog
+ * @age: age
+ * @owner: master
+ * Description: Structure for a dog
+ */
+
 struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-}
+};
 #endif
