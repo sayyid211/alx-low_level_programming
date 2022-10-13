5eb511224ae6678118a8e7a6ff35fdd1008af7c5
Adamu Muhammad <sayyid211@gmail.com>
2022-10-13 09:48:57 +0000

    iterate func ptrs

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -12,7 +12,7 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	size_t i; /* iterator val */
 
-	if (array == NULL)
+	if (array == NULL|| action == NULL)
 		return;
 	for (i = 0; i < size; i++)
 	{
