5d9747fe02bdb1097e0213d3c90d65fe0bff7a9a
Adamu Muhammad <sayyid211@gmail.com>
2022-10-13 10:26:32 +0000

    chect int

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include "function_pointers.h"
+
+/**
+ * int_index - searches for an int in a given set of inputs
+ * @array: array to store args
+ * @size: size of the array
+ * @cmp: pointer to function checking for ints
+ */
+
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i; /* iterator */
+
+	if (size <= 0)
+		return (-1);
+	for (i = 0; i < size; i++)
+	{
+		if (cmp(array[i]) != 0)
+			return (array[i]);
+	}
+	return (-1);
+}
