8e6e253c7e38c252a6b310856a92ff2c438d26c1
Adamu Muhammad <sayyid211@gmail.com>
2022-10-13 10:46:16 +0000

    check for ints

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -6,6 +6,7 @@
  * @array: array to store args
  * @size: size of the array
  * @cmp: pointer to function checking for ints
+ * Return: index of first int
  */
 
 int int_index(int *array, int size, int (*cmp)(int))
@@ -13,6 +14,8 @@ int int_index(int *array, int size, int (*cmp)(int))
 	int i; /* iterator */
 
 	if (size <= 0)
+		return (-1);
+	if (!cmp || ! array)
 		return (-1);
 	for (i = 0; i < size; i++)
 	{
