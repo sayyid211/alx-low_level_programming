db34cc866af50966487e5db0069460133a0277b2
Adamu Muhammad <sayyid211@gmail.com>
2022-10-14 13:16:30 +0000

    print all

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -18,38 +18,42 @@ void print_all(const char * const format, ...)
 	char *separator = "";
 
 	va_start(val, format);
-	while (format[counter])
+	if (format)
 	{
-		switch (format[counter])
+		while (format[counter])
 		{
-		case 'c':
-			p = va_arg(val, int);
-			printf("%s%c", separator, p);
-			break;
-		case 'f':
-			q = va_arg(val, double);
-			printf("%s%f", separator, q);
-			break;
-		case 'i':
-			r = va_arg(val, int);
-			printf("%s%d", separator, r);
-			break;
-		case 's':
-			s = va_arg(val, char *);
-			if (s == NULL)
+			switch (format[counter])
 			{
-				printf("(nil)");
+			case 'c':
+				p = va_arg(val, int);
+				printf("%s%c", separator, p);
+				break;
+			case 'f':
+				q = va_arg(val, double);
+				printf("%s%f", separator, q);
+				break;
+			case 'i':
+				r = va_arg(val, int);
+				printf("%s%d", separator, r);
+				break;
+			case 's':
+				s = va_arg(val, char *);
+				if (s == NULL)
+				{
+					printf("(nil)");
+				}
+				else
+				{
+					printf("%s%s", separator, s);
+				}
+				break;
+			default:
+				break;
 			}
-			else
-			{
-				printf("%s%s", separator, s);
-			}
-			break;
-		default:
-			break;
+			separator = ", ";
+			counter++;
 		}
-		separator = ", ";
-		counter++;
 	}
+	va_end(val);
 	printf("\n");
 }
