b57d9c9e15ca05a9eed79aec5c0c01049ec126aa
Adamu Muhammad <sayyid211@gmail.com>
2022-10-22 17:01:49 +0000

    correct count

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -1,7 +1,7 @@
 #include "lists.h"
 
 /**
- * list_len: counts length of str
+ * list_len - counts length of str
  * @h: pointer to node 0
  * Return: nodes count
  */
@@ -9,11 +9,12 @@
 size_t list_len(const list_t *h)
 {
 	size_t count = 0;
+
 	if (h == NULL)
 		return (0);
 	while (h->next != NULL)
 	{
-		h= h->next;
+		h = h->next;
 		count++;
 	}
 	return (count + 1);
