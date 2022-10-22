b66ea000873e2eb26f67e511248747b72af17d36
Adamu Muhammad <sayyid211@gmail.com>
2022-10-22 01:35:49 +0000

    print list

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,6 +1,9 @@
 #ifndef LISTS_H
 #define LISTS_H
 #include <stdlib.h>
+#include <strings.h>
+#include <stdio.h>
+
 int _putchar(char c);
 size_t print_list(const list_t *h);
 size_t list_len(const list_t *h);
