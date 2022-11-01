0401e3c7e6234b44752b505228621b0fc6c843ad
Adamu Muhammad <sayyid211@gmail.com>
2022-11-01 16:20:39 +0000

    create file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -28,16 +28,25 @@ int _strlen(char *s)
 int create_file(const char *filename, char *text_content)
 {
 	int fd;
-	int size = 1 + _strlen(text_content);
+	int txt;
+	int size = _strlen(text_content);
 
 	if (!filename)
 		return (-1);
 
 
-	fd = open("filename", O_TRUNC| O_CREAT| O_WRONLY, 0600);
-	if (!fd)
+	fd = open(filename, O_TRUNC | O_CREAT | O_WRONLY, 0600);
+	if (fd < 0)
 		return(-1);
-	if (text_content != NULL)
-		write(fd, text_content, size);
+	if (text_content)
+	{
+		txt = write(fd, text_content, size);
+		if (txt < 0)
+		{
+			close(fd);
+			return (-1);
+		}
+	}
+	close(fd);
 	return (1);
 }
