9448819ab68ce6b56735fbcfe717f12a496c24ef
Adamu Muhammad <sayyid211@gmail.com>
2022-12-10 16:29:11 +0000

    betty header

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -18,11 +18,9 @@ struct dog
 /**
  * dog_t - typedef for dog struct
  */
-
 typedef struct dog dog_t;
 
-void init_dog(struct dog *d, char *name, float age, char *owner\
-	);
+void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(dog_t *d);
