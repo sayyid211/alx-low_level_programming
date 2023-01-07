77beaea65de70df580c9944383f9c704ea09f83e
Adamu Muhammad <sayyid211@gmail.com>
2023-01-07 09:20:11 +0000

    Update: create file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -40,6 +40,8 @@ int create_file(const char *filename, char *text_content)
 		return (-1);
 	if (text_content)
 		txt = write(fd, text_content, size);
+	if (text_content == NULL)
+		return (1);
 	if (txt == -1)
 	{
 		close(fd);
