5dba550d1aab2c4aa81763d3ede8fcdf2dac3fdb
Adamu Muhammad <sayyid211@gmail.com>
2023-02-28 17:13:24 +0000

    strncpy updated

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -11,19 +11,16 @@
 char *_strncpy(char *dest, char *src, int n)
 {
 	int i = 0; /* iterator fot indeces */
+	int idx = 0; /* index length*/
 
 
-	while (i < n)
-	{
-		if (*(src + i) != '0')
-		{
-			dest[i] = src[i];
-			i++;
-		}
-		else
-		{
-			break;
-		}
-	}
+	while (src[i++])
+		idx++;
+	for (i = 0; src[i] && i < n; i++)
+		dest[i] = src[i];
+
+	for (i = idx; i < n; i++)
+		dest[i] = '\0';
+
 	return (dest);
 }
