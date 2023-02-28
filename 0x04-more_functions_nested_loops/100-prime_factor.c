e4ad01130e6f00566aece8a4b51e149d614d7ffa
Adamu Muhammad <sayyid211@gmail.com>
2023-02-28 12:10:01 +0000

    prime factor max

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Return: success
+ */
+
+int main(void)
+{
+	long n, div;
+
+	n = 612852475143;
+
+	for (div = 2; div < n; div++)
+	{
+		while (n % div == 0)
+		{
+			n = n/div;
+		}
+	}
+	printf("%lu\n", div);
+	return (0);
+}
