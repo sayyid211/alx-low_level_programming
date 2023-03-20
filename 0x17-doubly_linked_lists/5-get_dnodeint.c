180d9ea2c3ec11f0e54daf630d983e06ec1d43c9
Adamu Muhammad <sayyid211@gmail.com>
2023-03-20 00:31:43 +0000

    nth n ode

---
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * get_dnodeint_at_index - Return the nth node of a linked list
+ *
+ * @head: List head
+ * @index: nth node to get
+ * Return: pointer to the nth node
+ **/
+
+dlistint_t *get_dnode_at_index(dlistint_t *head, unsigned int index)
+{
+	size_t idx;
+
+	for (idx = 0; head != NULL; head = head->next, idx++)
+	{
+		if (idx == index)
+			return (head);
+	}
+	return (NULL);
+}
